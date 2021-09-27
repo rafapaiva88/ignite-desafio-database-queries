95818fa983aacbd97d37db9e1d65310bef12cbf8
Rafael Paiva <raffaelgustavo@hotmail.com>
2021-09-27 09:06:12 -0300

    desafio completo

---
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -0,0 +1,35 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { User } from '../../../users/entities/User';
+import { Game } from '../../entities/Game';
+
+import { IGamesRepository } from '../IGamesRepository';
+
+export class GamesRepository implements IGamesRepository {
+  private repository: Repository<Game>;
+
+  constructor() {
+    this.repository = getRepository(Game);
+  }
+
+  async findByTitleContaining(param: string): Promise<Game[]> {
+    return this.repository
+      .createQueryBuilder('games')
+      .where('LOWER(games.title) like LOWER(:title)', { title: `%${param}%` })
+      .getMany()
+  }
+
+  async countAllGames(): Promise<[{ count: string }]> {
+    return this.repository.query(`
+      SELECT count(*) FROM games AS count
+    `);
+  }
+
+  async findUsersByGameId(id: string): Promise<User[]> {
+    return this.repository
+      .createQueryBuilder()
+      .relation(Game, 'users')
+      .of(id)
+      .loadMany()
+  }
+}
